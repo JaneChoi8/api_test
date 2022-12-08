77ce1bc67125dc3ac243c1f0392188282ada824b
janechoi8 <janechoi2001@gmail.com>
2022-12-08 16:22:32 +0700

    update

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const app = express();
 const PORT = 6253;
 
+app.use(bodyParser.json())
+app.use(cors());
+
 app.listen(PORT, () => console.log (`Server running on port ${PORT}`));
-const users = {
-    firstname: 'Jayden Kor',
-    age: "25"
-}
+
+const users = [
+    {
+        firstname: 'Jayden',
+        lastname: 'Kor',
+        age: 25
+    },
+    {
+        firstname: 'Michelle',
+        lastname: 'Kor',
+        age: 25
+    },
+]
 
 app.get('/user', (req, res) => {
     res.send(users);
 });
 
-app.post('/api', (req, res) => {
-    res.send('Post is ready');
+app.post('/user', (req, res) => {
+    console.log('reached');
+    console.log(req.body);
+
+    const user = req.body;
+    users.push(user);
+
+    res.send('reached');
 })
 
-app.delete('/api/:id', (req, res) => {
+app.delete('/user/:id', (req, res) => {
     const {id} = req.params;
     res.send(`This id: ${id} is removed`);
 })
